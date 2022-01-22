4959ee34c6c898adead18848ea72124efc208805
Sorrow <Sorrow@SW-ACER>
2022-01-22 08:51:15 +0100

    Add SCSS

--- nuxt.config.js
+++ nuxt.config.js
@@ -18,6 +18,12 @@ export default {
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
+    // Load a Node.js module directly (here it's a Sass file)
+    'bulma',
+    // CSS file in the project
+    '@/assets/css/main.css',
+    // SCSS file in the project
+    '@/assets/css/main.scss',
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
